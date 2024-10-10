a3aed7136bb3d3da43da3fec7279f08613f8e6ba
minisangmini <sangmin5915@gmail.com>
2024-10-10 01:57:59 +0000

    feat: Add cors and global api prefix

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const config = new DocumentBuilder()
